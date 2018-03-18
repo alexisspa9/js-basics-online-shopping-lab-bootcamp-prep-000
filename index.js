1a1a64c985a6f26b312fe92050d1c3a403cad7e4
Alexander Spanos <alexisspa9@gmail.com>
2018-03-18 20:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === null) {
+  if (cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you")
   }
   else {
